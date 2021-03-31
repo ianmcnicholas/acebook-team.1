76c91e3e0e7939f19a7c0c5b413335391501cb16
Anna Nawrocka <anna.nawrocka1988@gmail.com>
2021-03-31 16:09:53 +0100

    Add  dependent: :destroy to user and post models

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
   # end 
 
   def destroy
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:id])   
     @post.destroy
     redirect_to root_path
   end
